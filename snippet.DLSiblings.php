d7b30715481176c8ce2968bf704114e8ba9beedb
Aharito <harand@mail.ru>
2017-04-03 13:54:19 +0700

    Fix

--- snippet.DLSiblings.php
+++ snippet.DLSiblings.php
@@ -4,9 +4,9 @@
  * вывод соседних ресурсов с шаблонизацией (множественная кольцевая перелинковка)
  * @category snippet
  *
- * @version   0.1
+ * @version   0.2
  * CMS version MODx Evo 7.1.6
- * @lastupdate 25/03/2017
+ * @lastupdate 03/04/2017
  * 
  * @author Aharito http://aharito.ru на основе DLPrevNext @author Agel_Nash <Agel_Nash@xaker.ru>
  *
@@ -27,112 +27,50 @@
  *       [[DLSiblings? &idType=`parents` &parents=`[*parent*]` &tpl=`@CODE: <a href="[+url+]">[+tv_h1+]</a><br>` &Qty=`2` &tvList=`h1` ]]
 **/
 
+
 if ( ! defined('MODX_BASE_PATH')) { die('HACK???'); }
 
-$ownerTPL = isset($ownerTPL) ? $ownerTPL : '@CODE:[+wrap+]'; //Дефолтное значение &ownerTPL
-$Qty = isset($Qty) ? $Qty : 2; //Дефолтное значение &Qty
-
-$out = "";
-$siblings = array();
-
-$ID = $modx->documentIdentifier;
+// Получаем параметры, заданные при вызове сниппета  DLSiblings
 $params = is_array($modx->Event->params) ? $modx->Event->params : array();
-$params = array_merge( $params, array('api' => '1', 'debug' => '0', 'display' => 'all') ); // 'display' => 'all', потому что за кол-во отвечает Qty 
-
-$json = $modx->runSnippet("DocLister", $params);
-$children = jsonHelper::jsonDecode($json, array('assoc' => true));
-$children = is_array($children) ? $children : array(); // Тут проверка, что вернулся массив
-
-$ids = array_keys($children); //Индексный массив ID в выборке (потом избавиться от него через prev-next?)
-
-$curIndex = array_search($ID, $ids); //Текущий индекс (индекс текущего ID)
-
-$count = count($ids); // Длина массива $ids
-$lastIndex = $count - 1; // Последний индекс массива $ids
-
-$TPL = DLTemplate::getInstance($modx);
-
-if($count-1 > 0) {// Если длина выборки (за исключением текущего элемента) больше 0
-
-	if(($count - 1) <= $Qty*2) { // Если длина выборки (за исключением текущего элемента) меньше нужного кол-ва
-		// То просто выводим все элементы выборки
-		for($i=0; $i<=$lastIndex; $i++) {
-			$out .= ($curIndex == $i) ? "" : $TPL->parseChunk($tpl, $children[$ids[$i]]);
-		}
-<?php
-/**
- * DLSiblings
- * вывод соседних ресурсов с шаблонизацией (множественная кольцевая перелинковка)
- * @category snippet
- *
- * @version   0.1
- * CMS version MODx Evo 7.1.6
- * @lastupdate 25/03/2017
- * 
- * @author Aharito http://aharito.ru на основе DLPrevNext @author Agel_Nash <Agel_Nash@xaker.ru>
- *
- * @param int &Qty Кол-во соседей с каждой стороны, default &Qty=`2`
- * @param string &ownerTPL Шаблон-обертка, должен содержать плейсхолдер [+wrap+], default &ownerTPL=`@CODE:<div>[+wrap+]</div>`
- * @NOTE остальные параметры - как у DocLister
- * @example
- *       [[DLSiblings? &idType=`parents` &parents=`[*parent*]` &tpl=`@CODE: <a href="[+url+]">[+tv_h1+]</a><br>` &Qty=`2` &tvList=`h1` ]]
-**/
-
-
-if ( ! defined('MODX_BASE_PATH')) { die('HACK???'); }
-
-include_once(MODX_BASE_PATH . 'assets/lib/APIHelpers.class.php');
-
-$p = &$modx->event->params;
-if ( ! is_array($p)) {
-    $p = array();
-}
 
 /**
- * Старым стилем :)
- * 
- * Некешир. сниппет на некешир. ресурсе
- * Mem : 3.5 mb, MySQL: 0.0140 s, 17 request(s), PHP: 0.1630 s, total: 0.1770 s, document from database
- *
- * $ownerTPL = isset($ownerTPL) ? $ownerTPL : '@CODE:[+wrap+]';			//Дефолтное значение &ownerTPL
- *
- * $prevQty = isset($Qty) ? $Qty : ( isset($prevQty) ? $prevQty : 2 );	//Дефолтное значение &prevQty
- * $nextQty = isset($Qty) ? $Qty : ( isset($nextQty) ? $prevQty : 2 );	//Дефолтное значение &nextQty
-**/
-
-
-/**
- * Новым DL-стилем :)
+ * Задаем дефолтные значения новым DL-стилем :)
  *
  * Некешир. сниппет на некешир. ресурсе
  * Mem : 3.5 mb, MySQL: 0.0190 s, 17 request(s), PHP: 0.1800 s, total: 0.1990 s, document from database
  *
- * Разница со старым способом в пределах погрешности 
-**/
+ * Разница со старым стилем (через isset) в пределах погрешности 
+ */
 
-$ownerTPL = \APIhelpers::getkey($p, 'ownerTPL', '@CODE:[+wrap+]');   //Дефолтное значение &ownerTPL
+// Шаблоны
+$ownerTPL = \APIhelpers::getkey($params, 'ownerTPL', null);
+$noneTPL = \APIhelpers::getkey($params, 'noneTPL', null);
+$tpl = \APIhelpers::getkey($params, 'tpl', '@CODE:<a href="[+url+]">[+e_title+]</a>');
 
-$Qty = \APIhelpers::getkey($p, 'Qty', 2);             //Дефолтное значение &Qty
-$prevQty = \APIhelpers::getkey($p, 'prevQty', $Qty);  //Дефолтное значение &prevQty
-$nextQty = \APIhelpers::getkey($p, 'nextQty', $Qty);  //Дефолтное значение &nextQty
+// Параметры
+$Qty = \APIhelpers::getkey($params, 'Qty', 2);
+$prevQty = \APIhelpers::getkey($params, 'prevQty', $Qty);
+$nextQty = \APIhelpers::getkey($params, 'nextQty', $Qty);
+$noneWrapOuter = \APIhelpers::getkey($params, 'noneWrapOuter', 1);
 
 
 $out = "";
 $siblings = array();
 
 $ID = $modx->documentIdentifier;
-$params = is_array($modx->Event->params) ? $modx->Event->params : array();
-$params = array_merge( $params, array('api' => '1', 'debug' => '0', 'display' => 'all') ); // 'display' => 'all', потому что за кол-во отвечает Qty 
 
+// мержим 'display' => '0' (выводить все док-ты), потому что за кол-во отвечает Qty, prevQty и nextQty
+$params = array_merge( $params, array('api' => '1', 'debug' => '0', 'display' => '0') );
+
+// Этот вызов ДокЛистера обрабатывает все наши параметры, кроме шаблонов и подстановки плейсхолдера [+sysKey.class+]
 $json = $modx->runSnippet("DocLister", $params);
 $children = jsonHelper::jsonDecode($json, array('assoc' => true));
 $children = is_array($children) ? $children : array(); // Тут проверка, что вернулся массив
 
 $ids = array_keys($children); //Индексный массив ID в выборке (потом избавиться от него через prev-next?)
-
 $curIndex = array_search($ID, $ids); //Текущий индекс (индекс текущего ID)
 
-$count = count($ids);    // Длина массива $ids
+$count = count($ids); // Длина массива $ids
 $lastIndex = $count - 1; // Последний индекс массива $ids
 
 $TPL = DLTemplate::getInstance($modx);
@@ -153,7 +91,7 @@ if($count-1 > 0) {// Если длина выборки (за исключени
 			 * Для Prev
 			 * Если "перескока" в хвост нет, то индекс вычисляется как $curIndex - $i
 			 * Если из начала $ids перескочили в его хвост, то индекс считаем как $count + $curIndex - $i
-			**/
+			 */
 			$index = ($curIndex - $i >= 0) ? $curIndex - $i : $count + $curIndex - $i;
 			
 			// Формируем массив $siblings с теми же индексами и значениями, как у $ids ($ids уже упорядочен как надо)
@@ -166,7 +104,7 @@ if($count-1 > 0) {// Если длина выборки (за исключени
 			 * Для Next
 			 * Если "перескока" на начало нет, то индекс вычисляется как $curIndex + $i
 			 * Если из хвоста $ids перескочили на его начало, то индекс считаем как $i - ($lastIndex - $curIndex) - 1
-			**/
+			 */
 			$index = ($curIndex + $i <= $lastIndex) ? $curIndex + $i : $i - ($lastIndex - $curIndex) - 1;
 			
 			// Дополняем массив $siblings с теми же индексами и значениями, как у $ids
@@ -177,35 +115,194 @@ if($count-1 > 0) {// Если длина выборки (за исключени
 		/**
 		 * В итоге $siblings - это индексный массив с пропусками индексов, значения - ID ресурсов
 		 * До сортировки выглядит примерно так: Array ( [6] => 114, [0] => 18, [5] => 109, [1] => 95 )
-		**/
+		 */
 
-		// Сортируем по индексам (ключам) этот небольшой массив $siblings (не более 4+4 элементов, а скорее всего 2+2 или 3+3)		
+		// Сортируем по индексам (ключам) этот небольшой массив $siblings (не более 8 элементов, а скорее всего 2+2 или 3+3)		
 		ksort($siblings);
 		
 		/**
 		 * После сортировки выглядит так: Array ( [0] => 18, [1] => 95, [5] => 109, [6] => 114 )
-	         * Теперь он отсортирован точно так же, как и было в выходных данных ДокЛистера
-		**/
+	     * Теперь он отсортирован точно так же, как и было в выходных данных ДокЛистера
+		 */
 
-		// Выводим все элементы $siblings с шаблонизацией
-		foreach($siblings as $value)
-			$out .= $TPL->parseChunk($tpl, $children[$value]);
+		/** Выводим все элементы $siblings с шаблонизацией
+		 * $i - номер итерации как в DL
+		 * @NOTE: @FIXME:??? В DL заложено неочевидное поведение: итерация считается с 0,
+<?php
+/**
+ * DLSiblings
+ * вывод соседних ресурсов с шаблонизацией (множественная кольцевая перелинковка)
+ * @category snippet
+ *
+ * @version   0.2
+ * CMS version MODx Evo 7.1.6
+ * @lastupdate 03/04/2017
+ * 
+ * @author Aharito http://aharito.ru на основе DLPrevNext @author Agel_Nash <Agel_Nash@xaker.ru>
+ *
+ * @param int &Qty Кол-во соседей с каждой стороны, имеет приоритет над &prevQty и &nextQty, default &Qty=`2`
+ * @param int &prevQty Кол-во соседей-предшественников. Приоритет меньше $Qty, default &prevQty=`2` 
+ * @param int &nextQty Кол-во соседей-последователей. Приоритет меньше $Qty, default &nextQty=`2` 
+ * @param string &ownerTPL Шаблон-обертка, должен содержать плейсхолдер [+wrap+], default &ownerTPL=`@CODE:<div>[+wrap+]</div>`
+ * @param string &tpl Шаблон элемента как в DocLister.
+ * @param string &noneTPL Шаблон с информацией, что ничего нет как в DocLister,  default - пусто.
+ * @param (0|1) &noneWrapOuter Как в DocLister, оборачивать ли шаблон noneTPL в обёртку ownerTPL.
+ * Параметр &noneWrapOuter имеет смысл, только если ничего не нашлось и при этом задан ownerTPL.
+ * @param string &prepare Как в DocLister.
+ * 
+ * @NOTE: Другие шаблоны из набора DocLister пока не используются.
+ * @NOTE: Остальные параметры - как у DocLister
+ *
+ * @example
+ *       [[DLSiblings? &idType=`parents` &parents=`[*parent*]` &tpl=`@CODE: <a href="[+url+]">[+tv_h1+]</a><br>` &Qty=`2` &tvList=`h1` ]]
+**/
+
+
+if ( ! defined('MODX_BASE_PATH')) { die('HACK???'); }
+
+// Получаем параметры, заданные при вызове сниппета  DLSiblings
+$params = is_array($modx->Event->params) ? $modx->Event->params : array();
+
+/**
+ * Задаем дефолтные значения новым DL-стилем :)
+ *
+ * Некешир. сниппет на некешир. ресурсе
+ * Mem : 3.5 mb, MySQL: 0.0190 s, 17 request(s), PHP: 0.1800 s, total: 0.1990 s, document from database
+ *
+ * Разница со старым стилем (через isset) в пределах погрешности 
+ */
+
+// Шаблоны
+$ownerTPL = \APIhelpers::getkey($params, 'ownerTPL', null);
+$noneTPL = \APIhelpers::getkey($params, 'noneTPL', null);
+$tpl = \APIhelpers::getkey($params, 'tpl', '@CODE:<a href="[+url+]">[+e_title+]</a>');
+
+// Параметры
+$Qty = \APIhelpers::getkey($params, 'Qty', 2);
+$prevQty = \APIhelpers::getkey($params, 'prevQty', $Qty);
+$nextQty = \APIhelpers::getkey($params, 'nextQty', $Qty);
+$noneWrapOuter = \APIhelpers::getkey($params, 'noneWrapOuter', 1);
+
+
+$out = "";
+$siblings = array();
+
+$ID = $modx->documentIdentifier;
+
+// мержим 'display' => '0' (выводить все док-ты), потому что за кол-во отвечает Qty, prevQty и nextQty
+$params = array_merge( $params, array('api' => '1', 'debug' => '0', 'display' => '0') );
+
+// Этот вызов ДокЛистера обрабатывает все наши параметры, кроме шаблонов и подстановки плейсхолдера [+sysKey.class+]
+$json = $modx->runSnippet("DocLister", $params);
+$children = jsonHelper::jsonDecode($json, array('assoc' => true));
+$children = is_array($children) ? $children : array(); // Тут проверка, что вернулся массив
+
+$ids = array_keys($children); //Индексный массив ID в выборке (потом избавиться от него через prev-next?)
+$curIndex = array_search($ID, $ids); //Текущий индекс (индекс текущего ID)
+
+$count = count($ids); // Длина массива $ids
+$lastIndex = $count - 1; // Последний индекс массива $ids
+
+$TPL = DLTemplate::getInstance($modx);
+
+if($count-1 > 0) {// Если длина выборки (за исключением текущего элемента) больше 0
+
+	if(($count - 1) <= $prevQty + $nextQty) { // Если длина выборки (за исключением текущего элемента) меньше нужного кол-ва
+		// То просто выводим все элементы выборки
+		for($i=0; $i<=$lastIndex; $i++) {
+			$out .= ($curIndex == $i) ? "" : $TPL->parseChunk($tpl, $children[$ids[$i]]);
+		}
+
+	} else { // Иначе ищем соседей
+
+		for($i=1; $i<=$prevQty; $i++) {
+
+			/** 
+			 * Для Prev
+			 * Если "перескока" в хвост нет, то индекс вычисляется как $curIndex - $i
+			 * Если из начала $ids перескочили в его хвост, то индекс считаем как $count + $curIndex - $i
+			 */
+			$index = ($curIndex - $i >= 0) ? $curIndex - $i : $count + $curIndex - $i;
+			
+			// Формируем массив $siblings с теми же индексами и значениями, как у $ids ($ids уже упорядочен как надо)
+			$siblings[$index] = $ids[$index];
+		}
+		
+		for($i=1; $i<=$nextQty; $i++) {
+
+			/**
+			 * Для Next
+			 * Если "перескока" на начало нет, то индекс вычисляется как $curIndex + $i
+			 * Если из хвоста $ids перескочили на его начало, то индекс считаем как $i - ($lastIndex - $curIndex) - 1
+			 */
+			$index = ($curIndex + $i <= $lastIndex) ? $curIndex + $i : $i - ($lastIndex - $curIndex) - 1;
+			
+			// Дополняем массив $siblings с теми же индексами и значениями, как у $ids
+			$siblings[$index] = $ids[$index];
+
+		}
+		
+		/**
+		 * В итоге $siblings - это индексный массив с пропусками индексов, значения - ID ресурсов
+		 * До сортировки выглядит примерно так: Array ( [6] => 114, [0] => 18, [5] => 109, [1] => 95 )
+		 */
+
+		// Сортируем по индексам (ключам) этот небольшой массив $siblings (не более 8 элементов, а скорее всего 2+2 или 3+3)		
+		ksort($siblings);
+		
+		/**
+		 * После сортировки выглядит так: Array ( [0] => 18, [1] => 95, [5] => 109, [6] => 114 )
+	     * Теперь он отсортирован точно так же, как и было в выходных данных ДокЛистера
+		 */
+
+		/** Выводим все элементы $siblings с шаблонизацией
+		 * $i - номер итерации как в DL
+		 * @NOTE: @FIXME:??? В DL заложено неочевидное поведение: итерация считается с 0,
+		 * а четность-нечетность определяется, как если бы итерация считалась с 1,
+		 * то есть документ, выводимый на итерации 0, считается нечетным, потому что он как бы 1-й по порядку.
+		 * Поэтому для документа, выводимого на итерации 0, действует tplId0 и tplOdd.
+		 *
+		 * Это логично, но при этом шаблон tplFirst применяется для $i==1, то есть для второго по порядку (см. коммент ниже)
+		 */
+		$i = 0;
+		
+		foreach($siblings as $value) {
+			$iterationName = ($i % 2 == 0) ? 'Odd' : 'Even';
+			
+			// Какой шаблон выводить на этой итерации?
+			$renderTPL = $tpl;
+			$renderTPL = \APIhelpers::getkey($params, 'tplId'.$i, $renderTPL);
+			$renderTPL = \APIhelpers::getkey($params, 'tpl'.$iterationName, $renderTPL);
+			if ($i == 0) {
+				// В DL здесь почему-то стоит $i==1 https://github.com/AgelxNash/DocLister/blob/master/assets/snippets/DocLister/core/DocLister.abstract.php#L1023
+				$renderTPL = \APIhelpers::getkey($params, 'tplFirst', $renderTPL);
+			}/*
+			if ($i == $lastIndex) {
+				$renderTPL = \APIhelpers::getkey($params, 'tplLast', $renderTPL);
+			}	*/		
+			
+			echo 'tpl'.$iterationName;	
+			$out .= $TPL->parseChunk($renderTPL, $children[$value]);			
+			$i++; // Увеличим $i на 1
+		}
 
 	}
         
-	// Оборачиваем в ownerTPL
-	$out = $TPL->parseChunk( $ownerTPL, array('wrap' => $out) );
+	// Оборачиваем в ownerTPL, если он не null
+	if( $ownerTPL )
+		$out = $TPL->parseChunk( $ownerTPL, array('wrap' => $out) );
 
 } else { // Если длина выборки (за исключением текущего элемента) <= 0 (нет элементов, кроме текущего, или вообще нет)
         
 	// Далее копируем поведение ДокЛистер для параметра &noneWrapOuter и шаблонов &noneTPL и &ownerTPL
 	
-	// Если задан noneTPL, парсим его без параметров
-	if(isset($params['noneTPL'])) $out = $TPL->parseChunk( $noneTPL, array() );
+	// Если noneTPL не null, парсим его без параметров
+	if( $noneTPL )
+		$out = $TPL->parseChunk( $noneTPL, array() );
 
-	// Если noneWrapOuter не задано или задано как 1, и ownerTPL не пустой
-	if( (!isset($params['noneWrapOuter']) || $params['noneWrapOuter'] == 1) && !empty($params['ownerTPL']) )
-		// то "нулевой" результат оборачиваем в ownerTPL
+	// Если noneWrapOuter не 0, и ownerTPL не null
+	if( $noneWrapOuter && $ownerTPL )
+		// то распарсенный noneTPL оборачиваем в ownerTPL
 		$out = $TPL->parseChunk( $ownerTPL, array('wrap' => $out) );
 
 }
